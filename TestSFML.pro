fe5199e98c77f7a2c51639266e3c405e3dcf37ab
Tony Lim <atomictheorist@gmail.com>
2015-03-16 23:44:16 -0700

    try re-draw RenderTexture

--- TestSFML.pro
+++ TestSFML.pro
@@ -1,14 +1,17 @@
 TEMPLATE = app
-CONFIG += console
-CONFIG -= app_bundle
-CONFIG -= qt
-
 SOURCES += main.cpp
 
 include(deployment.pri)
 qtcAddDeployment()
 
-INCLUDEPATH += "C:/sfml-2.2/include/"
-LIBS += -L"C:/sfml-2.2/lib/"
-CONFIG(debug, debug|release): LIBS += -lsfml-graphics-d -lsfml-window-d -lsfml-audio-d -lsfml-network-d -lsfml-system-d
+#LIBS
+unix:LIBS += -L"/usr/local/lib"
+win32:LIBS += -L"C:/sfml-2.2/lib/"
+
+# Include
+unix:INCLUDEPATH += "/usr/local/include"
+win32:INCLUDEPATH += "C:/sfml-2.2/include/"
+
+#CONFIG(debug, debug|release): LIBS += -lsfml-graphics-d -lsfml-window-d -lsfml-audio-d -lsfml-network-d -lsfml-system-d
+CONFIG(debug, debug|release): LIBS += -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system
 CONFIG(release, debug|release): LIBS += -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system
--- main.cpp
+++ main.cpp
@@ -3,41 +3,34 @@
 
 using namespace std;
 
-const int WINDOW_WIDTH=1000;
-const int WINDOW_HEIGHT=500;
+const int WINDOW_WIDTH=300;
+const int WINDOW_HEIGHT=300;
 const float RADIUS=5; //for CircleShape
 
-sf::Vector2f POINT1(200,200);
-sf::Vector2f POINT2(250,200);
-
-sf::Vector2f POINT3(50,50);
-sf::Vector2f POINT4(100,50);
-
-// test comment
+sf::Vector2f POINT1(150,150); // first 2 points
+sf::Vector2f POINT2(200,150);
 
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "TEST TEST");
 
-    // test addding comment by atomic
-
     //--------------renderTexture---------------
-    sf::RenderTexture generatedTexture; //declares the RenderTexture
-    generatedTexture.create(WINDOW_WIDTH,WINDOW_HEIGHT); //MADANTORY. Creates a new canvas for the texture. Use the same dimensions you used for RenderWindow
-    sf::Sprite sprite (generatedTexture.getTexture()); //sprites needed to be able to draw the texture
+    sf::RenderTexture mainTexture; //declares the RenderTexture
+    mainTexture.create(WINDOW_WIDTH, WINDOW_HEIGHT); //MADANTORY. Creates a new canvas for the texture. Use the same dimensions you used for RenderWindow
+    mainTexture.setSmooth(true);
 
-    sf::RenderTexture anotherTexture;
-    anotherTexture.create(WINDOW_WIDTH,WINDOW_HEIGHT);
-    sf::Sprite anotherSprite(anotherTexture.getTexture());
-    //------------------------------------------
+    sf::RenderTexture altTexture;
+    mainTexture.create(WINDOW_WIDTH, WINDOW_HEIGHT);
+    mainTexture.setSmooth(true);
 
-
+//    sf::RenderTexture anotherTexture;
+//    anotherTexture.create(WINDOW_WIDTH, WINDOW_HEIGHT);
 
     //-------------Just Simple Shapes---------------
-    sf::CircleShape circle(RADIUS);
-    circle.setOrigin(RADIUS/2,RADIUS/2);
-    circle.move(POINT1);
+//    sf::CircleShape circle(RADIUS);
+//    circle.setOrigin(RADIUS/2,RADIUS/2);
+//    circle.move(POINT1);
 
     //    sf::RectangleShape rectangle(sf::Vector2f(200,200));
     //    rectangle.setOrigin(100,100);
@@ -46,23 +39,32 @@ int main()
     //-----------------------------------------------
 
 
+    mainTexture.clear(sf::Color::Transparent); //clears you a fresh canvas to draw on
+    altTexture .clear(sf::Color::Transparent);
+
     //-------------Lines----------------
-    sf::Vertex line[]={
-        sf::Vertex(POINT1),
-        sf::Vertex(POINT2),
-    };
+    sf::VertexArray first_line(sf::LinesStrip,2);
+    first_line[0].position = POINT1;
+    first_line[1].position = POINT2;
 
-    //----------------------------------
-
-
-    sprite.setOrigin(POINT1); //sets new origin, BUT will move everything to where (0,0) was previous. Need to compensate for movement.
-    sprite.move(POINT1); //compensates for moving origin
-    //sprite.rotate(90); //now we can rotate
-
-    anotherSprite.setOrigin(POINT2);
-    anotherSprite.move(POINT2);
+    mainTexture.draw(first_line); //draws a line into original texture
 
 
+    //------------------------------------------
+    sf::Sprite sprite        (mainTexture.getTexture()); //sprites needed to be able to draw the texture
+    sf::Sprite rotatingSprite(mainTexture.getTexture());
+    sf::Sprite testSprite    (altTexture. getTexture());
+
+    sprite.setOrigin(POINT1); // sets new origin, BUT will move everything to where (0,0) was previous.
+                              // Need to compensate for movement.
+    sprite.move(POINT1);      // compensates for moving origin
+
+    rotatingSprite.setOrigin(POINT1);
+    rotatingSprite.move(POINT1.x + 50, POINT1.y);
+    rotatingSprite.rotate(-90);
+
+    testSprite.setOrigin(POINT1);
+    testSprite.move(POINT1);
 
 
     while (window.isOpen())
@@ -72,42 +74,45 @@ int main()
         {
             if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Return))
                 window.close();
+            else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Num1)
+            {
+//                mainTexture.clear(sf::Color::Transparent);
+                altTexture.clear(sf::Color::Transparent);
+                altTexture.draw(sprite);
+                altTexture.draw(rotatingSprite);
+                testSprite.setTexture(altTexture.getTexture());
+//                rotatingSprite.setTexture(mainTexture.getTexture());
+//                rotatingSprite.setOrigin(POINT1);
+//                rotatingSprite.move(POINT1.x + 50, POINT1.y);
+//                rotatingSprite.rotate(-90); //now we can rotate
+//                rotatingSprite.move(POINT1.x + 80, POINT1.y);
+            }
         }
-
 
 
         //----------------save to texture---------------------
 
-        //        generatedTexture.draw(circle); //draws a circle into the texture and hence the sprite
-        //        generatedTexture.draw(rectangle);//draws a rectangle into the texture and hence the sprite
+        //        mainTexture.draw(circle); //draws a circle into the texture and hence the sprite
+        //        mainTexture.draw(rectangle);//draws a rectangle into the texture and hence the sprite
 
 
+//        mainTexture.draw(line,2,sf::Lines); //draws a line into original texture
+//        mainTexture.draw(circle);           //draws a circle into original texture
 
-        generatedTexture.clear(sf::Color::Transparent); //clears you a fresh canvas to draw on
-        anotherTexture.clear(sf::Color::Transparent); //clears you a fresh canvas to draw on
+//        sprite.rotate(1);        //rotates slowly as an example
+//        rotatingSprite.rotate(1); //rotates slowly as an example
 
-        generatedTexture.draw(line,2,sf::Lines); //draws a line into original texture
-        generatedTexture.draw(circle); //draws a circle into original texture
-
-        sprite.rotate(1); //rotates slowly as an example
-        anotherSprite.rotate(1);//rotates slowly as an example
-
-        anotherTexture.draw(sprite); //draws the sprite from the original texture into the new texture
+//        anotherTexture.draw(sprite); //draws the sprite from the original texture into the new texture
         //notice I didn't draw the line and circle again.
 
-        generatedTexture.display(); //saves drawings into texture and hence the sprite
-        anotherTexture.display();//saves drawings into texture and hence the sprite
-
+//        mainTexture.display(); //saves drawings into texture and hence the sprite
         //------------------------------------------------------
-
-
-
 
         window.clear(); //comment out for fun effects
 
         window.draw(sprite); //use the RenderWindow to draw the sprite.
-        window.draw(anotherSprite);// draws the other sprite
-
+        window.draw(rotatingSprite);// draws the other sprite
+        window.draw(testSprite);
         window.display();
 
 
